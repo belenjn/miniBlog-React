f7128c0e4427f44bb5629d2d27b47ad6bd853684
Belen <belenjarabanavas31@gmail.com>
2022-04-28 16:43:00 +0200

    custom hook

--- src/Home.js
+++ src/Home.js
@@ -1,25 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { BlogList } from "./BlogList";
+import {useFetch} from "./hooks/useFetch.js"; 
 
 export const Home = () => {
-  const [blogs, setBlogs] = useState(null);
-  const [isPending, setIsPending] = useState(true);
 
-  useEffect(
-    () => async () => {
-      const response = await fetch("http://localhost:8000/blogs");
+  const {data: blogs, isPending} = useFetch("http://localhost:8000/blogs");
 
-      if (!response.ok) {
-        const message = `An error has ocurred ${response.status}`;
-        throw new Error(message);
-      }
-
-      const responseJSON = await response.json();
-      setBlogs(responseJSON);
-      setIsPending(false);
-    },
-    []
-  );
 
   return (
     <div className="home">
---
+++ src/hooks/useFetch.js
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+
+export const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+
+  useEffect(
+    () => async () => {
+      const response = await fetch(url);
+
+      if (!response.ok) {
+        const message = `An error has ocurred ${response.status}`;
+        throw new Error(message);
+      }
+
+      const responseJSON = await response.json();
+      setData(responseJSON);
+      setIsPending(false);
+    },[url]);
+
+  return {data, isPending}
+};
