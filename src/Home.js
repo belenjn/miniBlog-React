6c8af43599b1d373e6d036340545361f3f3edf63
Belen <belenjarabanavas31@gmail.com>
2022-04-28 16:19:15 +0200

    fetch finished

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { BlogList } from "./BlogList";
 
 export const Home = () => {
+  const [blogs, setBlogs] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+
+  useEffect(
+    () => async () => {
+      const response = await fetch("http://localhost:8000/blogs");
+
+      if (!response.ok) {
+        const message = `An error has ocurred ${response.status}`;
+        throw new Error(message);
+      }
+
+      const responseJSON = await response.json();
+      setBlogs(responseJSON);
+      setIsPending(false);
+    },
+    []
+  );
+
   return (
     <div className="home">
-      <h2>Homepage</h2>
+      {isPending && <div>Loading...</div>}
+      {blogs && <BlogList blogs={blogs} title="All Blogs:" />}
     </div>
   );
 };
