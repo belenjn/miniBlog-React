93a9c2ae7b5148d555a742bd2e049eeebb8420fb
Belen <belenjarabanavas31@gmail.com>
2022-04-29 12:13:08 +0200

    update styles

--- src/navbar/Navbar.js
+++ src/navbar/Navbar.js
@@ -6,7 +6,7 @@ import "./Navbar.css";
 export const Navbar = () => {
   return (
     <nav className='navbar'>
-        <h1>The Belen's Blog</h1>
+        <h1>First Blog</h1>
         <div className='links'>
             <Link to="/">Home</Link>
             <Link to="/create">New Blog</Link>
